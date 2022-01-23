2b8fa588056f6c161d5b228f65646ec7f90bf0d1
sergioc0sta <sergiomdcoliveira@gmail.com>
2022-01-23 19:29:55 +0000

    feat(frontend): Add config eslint prettier and husky

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,13 @@
-import '../styles/globals.ts'
-import { ApolloProvider } from "@apollo/client";
-import client from "../apollo/apollo-client";
+import '../styles/globals.ts';
+import { ApolloProvider } from '@apollo/client';
+import client from '../apollo/apollo-client';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
     </ApolloProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
